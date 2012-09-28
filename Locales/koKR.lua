7c7fdb5d05d2fd43a48ee0d969c01450df4f7b15
phyber <phyber@wowace>
2012-09-28 19:14:05 +0100

    Update koKR locale. Thanks next96.

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -2,15 +2,24 @@
 if not L then return end
 --
 L["Profession Bags"] = "전문기술 가방"
-L["Bag Depletion"] = "빈 슬롯 표시"
 L["Include profession bags"] = "슬롯의 수를 계산할 때 전문기술 가방을 포함합니다"
+--
+L["Bag Depletion"] = "빈 슬롯 표시"
 L["Show depletion of bags"] = "가방의 사용 중인 슬롯의 수 대신에 빈 슬롯의 수를 표시합니다"
+--
 L["Bag Total"] = "총 슬롯수"
 L["Show total amount of space in bags"] = "가방안의 총 슬롯의 수를 표시합니다"
-
-L["Click|r to open your bags"] = "클릭시 가방 열기"
-
+--
+L["Click|r to open your bags"] = "클릭|r시 가방 열기"
+L["Right-Click|r to open options menu"] = "오른쪽 클릭|r시 설정메뉴 열기"
+--
 L["Open Bags at Bank"] = "은행 방문시 가방열기"
 L["Open all of your bags when you're at the bank"] = "은행을 클릭했을 때 모든 가방을 자동으로 엽니다."
 L["Open Bags at Vendor"] = "상인 방문시 가방열기"
 L["Open all of your bags when you're at a vendor"] = "상인을 클릭했을 때 모든 가방을 자동으로 엽니다."
+--
+L["Use Colours"] = "색상 사용"
+L["Use colouring to show level of bag fullness"] = "가방이 가득 찼을 때 레벨 색상을 사용합니다."
+--
+L["Bags in Tooltip"] = "가방 툴팁 표시"
+L["Show all bags in the Broker: Bags tooltip"] = "브로커에 모든 가방의 툴팁을 표시합니다."
