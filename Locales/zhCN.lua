1b14539988dccc50c8397f14601c743bdda4d4b3
phyber <phyber@wowace>
2012-10-17 15:07:21 +0100

    Updated zhCN translation. Thanks dh0000

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -1,14 +1,26 @@
 ﻿local L = LibStub("AceLocale-3.0"):NewLocale("Broker_BagFu", "zhCN")
 if not L then return end
 --
-L["Profession Bags"] = "专业背包"
-L["Bag Depletion"] = "背包已使用状态"
-L["Include profession bags"] = "包括专业背包"
-L["Show depletion of bags"] = "显示背包剩余空间"
+L["Profession Bags"] = "专业材料背包"
+L["Include profession bags"] = "包括专业材料背包"
+--
+L["Bag Depletion"] = "背包剩余空间"
+L["Show depletion of bags"] = "显示背包剩余的空间"
+--
 L["Bag Total"] = "背包总容量"
 L["Show total amount of space in bags"] = "显示背包总容量大小"
-
-L["Click|r to open your bags"] = "点击打开你的背包"
-
-L["Open Bags at Bank"] = "打开银行同时打开背包"
-L["Open all of your bags when you're at the bank"] = "当你打开银行的时候,同时打开所有背包"
+--
+L["Click|r to open your bags"] = "左键点击|r打开你的背包"
+L["Right-Click|r to open options menu"] = "右键点击|r打开设置面板"
+L["Ctrl-Click|r to open keyring"] = "Ctrl+左键点击|r打开钥匙链"
+--
+L["Open Bags at Bank"] = "打开银行时打开所有背包"
+L["Open all of your bags when you're at the bank"] = "当你打开银行的时候，同时打开所有背包"
+L["Open Bags at Vendor"] = "与商人交易时打开所有背包"
+L["Open all of your bags when you're at a vendor"] = "当你与商人交易的时候，同时打开所有背包"
+--
+L["Use Colours"] = "染色"
+L["Use colouring to show level of bag fullness"] = "使用不同的颜色来显示背包剩余空间状态"
+--
+L["Bags in Tooltip"] = "显示每个背包"
+L["Show all bags in the Broker: Bags tooltip"] = "在提示信息中单独显示每个背包的状态"
